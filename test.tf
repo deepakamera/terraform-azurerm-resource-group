e0e656ac9dabc7e869d382d74f0cea588de7ec6c
deepakamera <57987008+deepakamera@users.noreply.github.com>
2022-07-04 16:11:31 +0530

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,26 @@
+terraform
+{
+  required_version=">0.12.2"
+}
+#Configure the Azure provider
+provider "azurerm" {
+  version=">2.0"
+  features{}
+}
+variable "resource_group_name" 
+{
+  default="my-rg"
+  description="test"
+}
+variable "resource_group_location"
+{
+  default="westus"
+  description="test"
+}
+  
+#Creating a resource group
+resource "azurerm_resource_group""my"
+{
+  name= var.resource_group_name
+  location= var.resource_group_location
+}
