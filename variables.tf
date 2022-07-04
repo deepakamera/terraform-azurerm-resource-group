ed901217473ce8571b701f11aa2c60c3ed0469a1
deepakamera <57987008+deepakamera@users.noreply.github.com>
2022-07-04 13:35:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,26 +1,9 @@
-variable "location" {
-  description = "Azure Region"
-  type        = string
+variable "resource_group_name_prefix" {
+  default       = "rg"
+  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
-variable "tags" {
-  description = "Tags to be applied to resources (inclusive)"
-  type        = map(string)
-}
-
-variable "unique_name" {
-  description = "Freeform input to append to resource group name. Set to 'true', to append 5 random integers"
-  type        = string
-  default     = null
-}
-
-variable "names" {
-  description = "Names to be applied to resources (inclusive)"
-  type        = object({
-    environment         = string
-    location            = string
-    market              = string
-    product_name        = string
-    resource_group_type = string
-  })
+variable "resource_group_location" {
+  default = "eastus"
+  description   = "Location of the resource group."
 }
