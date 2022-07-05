a9d4a5610ffeb5bc7e8d95a1eaf41c79d77fdf00
deepakamera <57987008+deepakamera@users.noreply.github.com>
2022-07-05 16:29:54 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,27 +1,18 @@
 terraform {
-  required_version=">0.12.2"
-}
-#Configure the Azure provider
-provider "azurerm" {
-  version=">2.0"
-  features{}
-}
 
-
-variable "resource_group_name" 
-{
-  default="my-rg"
-  description="test"
-}
-variable "resource_group_location"
-{
-  default="westus"
-  description="test"
-}
+  required_version = ">=0.12"
   
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
 #Creating a resource group
 resource "azurerm_resource_group""my"
 {
-  name= var.resource_group_name
-  location= var.resource_group_location
+  name="test-rg"
+  location="eastus"
 }
