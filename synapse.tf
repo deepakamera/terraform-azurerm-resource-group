48df068466b8b301a184858ee3ae0bb9f204fcaf
deepakamera <57987008+deepakamera@users.noreply.github.com>
2022-07-06 17:13:42 +0530

    Create synapse.tf

---
+++ synapse.tf
@@ -0,0 +1,7 @@
+resource "azurerm_synapse_workspace" "example" {
+  name                                 = "test-synapse"
+  resource_group_name                  = "test-rg"
+  location                             = "eastus"
+  }
+
+ 
